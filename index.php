8281108acaae3ac7a328f99217fc4eb8248adc3d
teravia8726 <teravia8726@gmail.com>
2026-07-26 12:11:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
-<?php 
-declare(strict_types=1);
+<?php
 
-$page = $_GET['page'] ?? 'home';
-
-$allowedPages = [
-    'home',
-    'search',
-    'detail',
-    'login',
-    'register',
-    'membership',
-    'post-property'
-];
-
-if (!in_array($page, $allowedPages, true)) {
-    http_response_code(404);
-    exit('404 - Halaman tidak ditemukan');
-}
-
-// require_once __DIR__ . '/config/database.php';
-
-$view = __DIR__ . '/views/' . $page . '.php';
-
-if (!file_exists($view)) {
-    http_response_code(404);
-    exit('View tidak ditemukan');
-}
-
-require_once $view;
+echo "TERAVIA OK";
