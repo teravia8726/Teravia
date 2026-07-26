ccc311c60d616603266dbd55be73e58bc5b2ae09
teravia8726 <teravia8726@gmail.com>
2026-07-26 10:24:22 +0700

    Update header.php

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,1 +1,58 @@
+<header class="site-header">
 
+    <div class="container">
+
+        <a href="/" class="logo">
+            <img
+                src="/assets/branding/logo.png"
+                alt="TERAVIA"
+            >
+        </a>
+
+        <nav class="desktop-menu">
+
+            <a href="/">Beranda</a>
+
+            <a href="/views/search.php">
+                Cari Properti
+            </a>
+
+            <a href="/views/post-property.php">
+                Pasang Iklan
+            </a>
+
+            <a href="/views/membership.php">
+                Membership
+            </a>
+
+            <a href="#">
+                Blog
+            </a>
+
+            <a href="#">
+                Bantuan
+            </a>
+
+        </nav>
+
+        <div class="header-action">
+
+            <a
+                href="/views/login.php"
+                class="btn-login"
+            >
+                Masuk
+            </a>
+
+            <a
+                href="/views/register.php"
+                class="btn-register"
+            >
+                Daftar
+            </a>
+
+        </div>
+
+    </div>
+
+</header>
