c165c0e04a6926f6949d6aa6cb496dfb883feb25
teravia8726 <teravia8726@gmail.com>
2026-07-26 10:03:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<?php
-// index.php - Gerbang Utama Aman via JavaScript Redirect
-session_start();
-?>
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="0;url=/views/home.php">
-    <title>Memuat...</title>
-</head>
-<body>
-    <script>
-        window.location.href = "/views/home.php";
-    </script>
-</body>
-</html>
+
